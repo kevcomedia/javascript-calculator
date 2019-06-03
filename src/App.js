6a2ba0ae3336edeb0d9f961a88271861bcd23b04
Kev Comedia <kevcomedia@gmail.com>
2019-06-03 17:08:09 +0800

    Write app mockup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">0</div>
+      <div className="buttons">
+        <button className="btn btn_type_clear btn_size_wide" type="button">
+          AC
+        </button>
+        <button className="btn btn_type_operator" type="button">
+          /
+        </button>
+        <button className="btn btn_type_operator" type="button">
+          *
+        </button>
+        <button className="btn" type="button">
+          7
+        </button>
+        <button className="btn" type="button">
+          8
+        </button>
+        <button className="btn" type="button">
+          9
+        </button>
+        <button className="btn btn_type_operator" type="button">
+          -
+        </button>
+        <button className="btn" type="button">
+          4
+        </button>
+        <button className="btn" type="button">
+          5
+        </button>
+        <button className="btn" type="button">
+          6
+        </button>
+        <button className="btn btn_type_operator" type="button">
+          +
+        </button>
+        <button className="btn" type="button">
+          1
+        </button>
+        <button className="btn" type="button">
+          2
+        </button>
+        <button className="btn" type="button">
+          3
+        </button>
+        <button className="btn btn_type_equals btn_size_tall" type="button">
+          =
+        </button>
+        <button className="btn btn_size_wide" type="button">
+          0
+        </button>
+        <button className="btn" type="button">
+          .
+        </button>
+      </div>
     </div>
   );
 }
