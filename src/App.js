ee3aaa745489e60ec27bc798b89573452ebbca9b
Kev Comedia <kevcomedia@gmail.com>
2019-06-03 17:23:58 +0800

    Extract buttons div as its own component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
 import React from 'react';
 import Display from './Display';
+import CalculatorButtons from './CalculatorButtons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Display display="0" />
-      <div className="buttons">
-        <button className="btn btn_type_clear btn_size_wide" type="button">
-          AC
-        </button>
-        <button className="btn btn_type_operator" type="button">
-          /
-        </button>
-        <button className="btn btn_type_operator" type="button">
-          *
-        </button>
-        <button className="btn" type="button">
-          7
-        </button>
-        <button className="btn" type="button">
-          8
-        </button>
-        <button className="btn" type="button">
-          9
-        </button>
-        <button className="btn btn_type_operator" type="button">
-          -
-        </button>
-        <button className="btn" type="button">
-          4
-        </button>
-        <button className="btn" type="button">
-          5
-        </button>
-        <button className="btn" type="button">
-          6
-        </button>
-        <button className="btn btn_type_operator" type="button">
-          +
-        </button>
-        <button className="btn" type="button">
-          1
-        </button>
-        <button className="btn" type="button">
-          2
-        </button>
-        <button className="btn" type="button">
-          3
-        </button>
-        <button className="btn btn_type_equals btn_size_tall" type="button">
-          =
-        </button>
-        <button className="btn btn_size_wide" type="button">
-          0
-        </button>
-        <button className="btn" type="button">
-          .
-        </button>
-      </div>
+      <CalculatorButtons />
     </div>
   );
 }
